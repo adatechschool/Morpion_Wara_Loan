270100de44391a8ca4295d041144cd36402932a2
Wara-nyu <w.bounkeua@gmail.com>
2020-02-26 17:27:12 +0100

    ajout de getter setter pour le jeu.rb

--- affichage.rb
+++ affichage.rb
@@ -1,9 +1,11 @@
 require_relative "touche"
 require_relative "grille"
-require_relative "joueur"
+
+
 
 class Affichage
 
+  attr_accessor :grille1
 
 	def initialize
 		# le jeu se compose des items de la cases_array. Normalement si tu lances le fichier ruby, le morpion devrait s'afficher correctement
@@ -11,18 +13,19 @@ class Affichage
 		# je voulais aussi laisser un affichage avec la classe Player mais j'arrive pas a connecter mes classes
 		# donc je l'ai fait sans variable, mais la volonte etait la
 
-		grille1 = Grille.new
-
-		puts "----------------"
-		puts "| #{grille1.touche_array[0].valeur} | #{grille1.touche_array[1].valeur} | #{grille1.touche_array[2].valeur} |" 
-		puts "----------------"
-		puts "| #{grille1.touche_array[3].valeur} | #{grille1.touche_array[4].valeur} | #{grille1.touche_array[5].valeur} |"
-		puts "----------------"
-		puts "| #{grille1.touche_array[6].valeur} | #{grille1.touche_array[7].valeur} | #{grille1.touche_array[8].valeur} |"
-		puts "----------------"
-		puts "Joueur 1 = x"
-		puts "Joueur 2 = o"
-
+    @grille1 = Grille.new
 	end
 
+  def affiche
+		puts "-------------------"
+    puts "| #{@grille1.touche_array[0].valeur} | #{@grille1.touche_array[1].valeur} | #{@grille1.touche_array[2].valeur} |" 
+		puts "-------------------"
+		puts "| #{@grille1.touche_array[3].valeur} | #{@grille1.touche_array[4].valeur} | #{@grille1.touche_array[5].valeur} |"
+		puts "-------------------"
+		puts "| #{@grille1.touche_array[6].valeur} | #{@grille1.touche_array[7].valeur} | #{@grille1.touche_array[8].valeur} |"
+		puts "-------------------"
+  end
+
+  
 end
+
--- jeu.rb
+++ jeu.rb
@@ -1,42 +1,53 @@
 require_relative "joueur"
 require_relative "affichage"
-
+require_relative "touche"
 class Jeu
-
-    attr_accessor :affichage
 
     def initialize
 
         system "clear"
         puts "Bienvenue dans le jeu de morpion de Loan et Wara"
-        sleep 1
-        system "clear"
+        #sleep 1
+        #system "clear"
 
         puts "Joueur 1, comment tu t'appelles ?"
         prenom1 = gets.chomp
-        joueur1 = Joueur.new(prenom1, "x")
-        puts "Salut #{joueur1.nom}, tu seras #{joueur1.signe}"
-        sleep 2
-        system "clear"
+        @joueur1 = Joueur.new(prenom1, "x")
+        puts "Salut #{@joueur1.nom}, tu seras #{@joueur1.signe}"
+        #sleep 2
+        #system "clear"
 
         puts "Joueur 2, comment tu t'appelles ?"
         prenom2 = gets.chomp
-        joueur2 = Joueur.new(prenom2, "o")
-        puts "Salut #{joueur2.nom}, tu seras #{joueur2.signe}"
-        sleep 2
-        system "clear"
+        @joueur2 = Joueur.new(prenom2, "o")
+        puts "Salut #{@joueur2.nom}, tu seras #{@joueur2.signe}"
+        #sleep 2
+        #system "clear"
 
-        Affichage.new
+        @nouvelleA = Affichage.new
+        @nouvelleA.affiche
+        puts "#{@joueur1.nom} = x"
+        puts "#{@joueur2.nom} = o"
 
     end
-
-
-
-
-
-
+    def tour
+      puts "#{@joueur1.nom}, à toi de jouer"
+      choix = gets.chomp.to_s
+=begin
+      touche_array.each do |touche|
+        if touche.position == choix
+          touche.valeur == " x "
+        elsif choix != touche.position
+          return False
+        end
+=end
+      if choix == "a1"
+        puts @nouvelleA.grille1.touche_array[0].valeur
+        @nouvelleA.grille1.touche_array[3].valeur = " x "
+        @nouvelleA.affiche
+      end
+    end
 end
 
 jeu1 = Jeu.new
-
-
+jeu1.tour

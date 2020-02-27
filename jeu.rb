ce1bac5e5eb4ce7a7bf4cea9b252de631eced861
Jane Lane <loan.does@gmail.com>
2020-02-27 17:05:44 +0100

    ajout victory

--- jeu.rb
+++ jeu.rb
@@ -28,17 +28,18 @@ class Jeu
         @affichage1 = Affichage.new
         puts "#{@joueur1.nom} = x"
         puts "#{@joueur2.nom} = o"
-
     end 
 
     def tour
+    
         puts "#{@joueur1.nom}, à toi de jouer"
         valide = false
-        while valide == false
+
+        while valide == false do
             choix = gets.chomp.to_s
             @affichage1.grille1.touche_array.each do |touche|             
                 if touche.valeur == choix && choix != "x" && choix != "o"
-                    touche.valeur = "x" 
+                    touche.valeur = "x"
                     valide = true
                     @affichage1.affiche
                 end
@@ -48,9 +49,11 @@ class Jeu
             end
         end
 
+        self.victory
+
         puts "#{@joueur2.nom}, à toi de jouer"
         valide = false
-        while valide == false
+        while valide == false do
             choix = gets.chomp.to_s
             @affichage1.grille1.touche_array.each do |touche|             
                 if touche.valeur == choix && choix != "x" && choix != "o"
@@ -63,13 +66,31 @@ class Jeu
             puts "Veuillez rentrer une touche valide"
             end
         end
+        self.victory
     end
 
-end 
+    def victory
+        touch = @affichage1.grille1.touche_array
+        if touch[0].valeur == touch[1].valeur && touch[1].valeur == touch[2].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[3].valeur == touch[4].valeur && touch[4].valeur == touch[5].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[6].valeur == touch[7].valeur && touch[7].valeur == touch[8].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[0].valeur == touch[3].valeur && touch[3].valeur == touch[6].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[1].valeur == touch[4].valeur && touch[4].valeur == touch[7].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[2].valeur == touch[5].valeur && touch[5].valeur == touch[8].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[0].valeur == touch[4].valeur && touch[4].valeur == touch[8].valeur
+            puts "Bravo, vous avez gagné !"
+        elsif touch[2].valeur == touch[4].valeur && touch[4].valeur == touch[6].valeur
+            puts "Bravo, vous avez gagné !"
+        end  
+    end
 
-jeu1 = Jeu.new
-jeu1.tour
-jeu1.tour
-jeu1.tour
-jeu1.tour
+end
 
+jeu1 = Jeu.new 
+jeu1.tour
